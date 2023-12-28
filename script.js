2b8dcd4920331886c9a4c63ad92fa0044872ae2d
Carson <carsonjohnbain@gmail.com>
2023-12-27 21:24:01 -0800

    add JS file

---
+++ script.js
@@ -0,0 +1,7 @@
+// const button = document.querySelector('button');
+
+// button.addEventListener('click', () => {
+//     button.style.background = '#485739';
+// })
+
+// // trying to figure out button change of color 
